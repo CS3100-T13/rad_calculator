6124695a502a5143f40d4ac427f74b9158f86f74
grbcp5 <grbcp5@mst.edu>
2018-04-04 11:34:00 -0500

    Initial Commit

---
+++ app/src/main/java/edu/mst/cs3100/s18/t13/calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.mst.cs3100.s18.t13.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
